e9f1a92ca79c620d28e88738a60bd08675a84901
gersonlaparra13@gmail.com <estudiantev@COED.LOCAL>
2025-08-25 15:19:09 -0600

    Modifique el primer commit

--- Base De Datos.py
+++ Base De Datos.py
@@ -1,17 +1,20 @@
 import sqlite3
 
-def ConectarBase():
-    baseDeDatos = sqlite3.connect("Juego_Ahorcado.db")
-    cr = baseDeDatos.cursor()
-    cr.execute('''
-        CREATE TABLE IF NOT EXISTS palabras (
+def Conectar():
+    baseDeDatos= sqlite3.connect("Ahorcado.db")
+    cursor =  baseDeDatos.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS palabras (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            palabra TEXT NOT NULL UNIQUE,
-            descripcion TEXT NOT NULL
- )
+            palabra TEXT NOT NULL,
+            descripcion TEXT NOT NULL)
 ''')
     baseDeDatos.commit()
     baseDeDatos.close()
+
+
+
+
 
 
 
