92a7e74d2eee4a4ca9acdf190a6b068b262360db
gersonlaparra13@gmail.com <estudiantev@COED.LOCAL>
2025-09-01 15:15:35 -0600

    Modifique codigo de prueva

--- Base De Datos.py
+++ Base De Datos.py
@@ -55,10 +55,10 @@ def Listar_P():
     baseDeDatos.close()
     return datos
 
-   if __name__ == "__main__":
+if __name__ == "__main__":
      conectar()
      Agregar_P("Perro", "Animal doméstico conocido como el mejor amigo del hombre.")
-        print("Palabra Aleatoria:", Obtener_P_A())
+     print("Palabra Aleatoria:", Obtener_P_A())
 
 
 
